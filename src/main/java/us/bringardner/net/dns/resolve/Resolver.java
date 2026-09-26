7274bffcb284d44634382816a3de851fda95037c
tony-bringardner <tony.bringardner@gmail.com>
2026-09-26 10:06:01 -0400

    Cleanup

--- src/main/java/us/bringardner/net/dns/resolve/Resolver.java
+++ src/main/java/us/bringardner/net/dns/resolve/Resolver.java
@@ -31,7 +31,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
